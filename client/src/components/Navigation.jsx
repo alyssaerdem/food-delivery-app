4ac98e1c5655ebe96915695fb4bc2f2141082016
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2022-08-22 16:40:53 +0300

    updated responsiveness of nav and footer

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "../styles/Navigation.module.css"
 import { FaShoppingCart } from 'react-icons/fa';
 import Logo from "../logo.png";
-
 import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
 
 const Navigation = () => {
+
 
     const scrollDown = () => {
         console.log("here")
@@ -15,9 +17,15 @@ const Navigation = () => {
       });
     };
 
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const toggleOpenMenu = () => {
+        setOpenMenu(!openMenu);
+    }
+
     return (
     <div className={styles.container}>
-        <div className={styles.item}>
+        <div className={styles.logoDiv}>
         <img src={Logo} alt="Pizza Bella logo" className={styles.logo}/> 
         </div>
         <div className={styles.item}>
@@ -29,22 +37,46 @@ const Navigation = () => {
                 <Link to="/products" className={styles.link}>Products</Link>
             </li>
             <li className={styles.listItem}>
-                <p className={styles.link} onClick={()=> scrollDown()}>About</p>
+                <p className={styles.pLink} onClick={()=> scrollDown()}>About</p>
             </li>
-         </ul>
+            </ul>
          </div>
         <div className={styles.item}>
             <div className={styles.cart}>
             <Link to="/cart" className={styles.link}>
                 <FaShoppingCart className={styles.cartIcon}/>
                 <div className={styles.counter}>1</div>
-            </Link>
-            {/*} <Link to="/login" className={styles.link}>
-                <FaUserCircle className={styles.userIcon}/>
-    </Link> */}
-    </div>
+            </Link>   
         </div>
-    </div>
+        </div>
+        <div className={styles.sideMenu}>
+        <div>
+            {!openMenu ? <GiHamburgerMenu onClick={toggleOpenMenu} className={styles.hamburgerIcon}/> :
+            <GrClose onClick={toggleOpenMenu} className={styles.hamburgerIcon}/> }
+            <div className={styles.openMenu} style={{ display: (openMenu ? 'block' : 'none') }}>
+                <div className={styles.sideItem}>
+                    <Link to="/" className={styles.link}>Home</Link>
+                </div>
+            <div className={styles.sideItem}>
+                <Link to="/products" className={styles.link}>Products</Link>
+            </div>
+            <div className={styles.sideItem}>
+                <p className={styles.pLink} onClick={()=> scrollDown()}>About</p>
+            </div>
+
+            <div className={styles.sideItem}>
+                <Link to="/cart" className={styles.link}>
+                     Cart&nbsp;
+                    <FaShoppingCart className={styles.cartIcon}/>
+                    <div className={styles.counter}>1</div>
+                </Link>  
+            </div>
+            </div>
+        </div>
+        </div>
+        
+       </div>
+      
     )
 };
 
