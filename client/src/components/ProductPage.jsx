a1dfa25410101d243887f8c33ee1c44ee0d112e0
Alyssa Newhart <67665773+alyssanewhart@users.noreply.github.com>
2022-09-12 16:56:04 +0300

    update product page

--- client/src/components/ProductPage.jsx
+++ client/src/components/ProductPage.jsx
@@ -8,40 +8,53 @@ const ProductPage = () => {
 const location = useLocation();
 const product = location.state.data.product;
 const [selectedSize, setSelectedSize] = useState("");
+
 console.log(product)
     return (
         <div className={styles.container}>
-            <div className={styles.productInfo}>
             <div className={styles.item}>
-            <h1 className={styles.nameH1}>{product.name}</h1>
+            <h1 className={styles.h1}>{product.name}</h1>
             <div className={styles.imgDiv}>
-                <img src={PizzaImage} alt="margherita pizza" className={styles.productImg}/>
+                <img src={PizzaImage} alt="margherita pizza" className={styles.image}/>
             </div>
             <div className={styles.description}>
-               <h3>Ingredients</h3>
+
                <p className={styles.descriptionText}>
                 {product.description} 
                 </p>
             </div>
             </div>
             <div className={styles.item}>
-                <div className={styles.sizeH2}><h2>Sizes</h2></div>
+                <div className={styles.h2}><h2>Sizes</h2></div>
             <div className={styles.sizeContainer}>
             {product.sizes.map(element => {
                 console.log(element)
                         let size = element.size;
                         let price = element.price;
                         switch(size) {
-                            case 'Small':  return <div className={selectedSize === size ? styles.selectedSize : styles.sizeItem} onClick={() => setSelectedSize(size)}><span className={styles.size}>${price}</span> <GiFullPizza className={styles.smallPizza}/>{size}</div>
-                            case 'Medium': return <div className={selectedSize === size ? styles.selectedSize : styles.sizeItem} onClick={() => setSelectedSize(size)}><span className={styles.size}>${price}</span><GiFullPizza className={styles.mediumPizza}/>{size}</div>
-                            case 'Large':  return <div className={selectedSize === size ? styles.selectedSize : styles.sizeItem} onClick={() => setSelectedSize(size)}><span className={styles.size}>${price}</span><GiFullPizza className={styles.largePizza}/>{size}</div>
+                            case 'Small':  return <div className={selectedSize === size ? styles.selectedSize : styles.sizeItem} onClick={() => setSelectedSize(size)}><span className={styles.size}>${price}</span><GiFullPizza className={styles.smallPizza}/><p>{size}</p></div>
+                            case 'Medium': return <div className={selectedSize === size ? styles.selectedSize : styles.sizeItem} onClick={() => setSelectedSize(size)}><span className={styles.size}>${price}</span><GiFullPizza className={styles.mediumPizza}/><p>{size}</p></div>
+                            case 'Large':  return <div className={selectedSize === size ? styles.selectedSize : styles.sizeItem} onClick={() => setSelectedSize(size)}><span className={styles.size}>${price}</span><GiFullPizza className={styles.largePizza}/><p>{size}</p></div>
                             default: return <div>Size not available</div>
                         }
             })}
             </div>
-            </div>
-            </div>
+            <div className={styles.quantityContainer}>
+                <form>
+                <label className={styles.label}>
+                    Quantity: 1
+                  {/*  <button className={styles.inputBtn}>-</button>
+                    <p>{quantity}</p>
+                    <button className={styles.inputBtn}>+</button>*/}
+        </label>    
+            </form>  
+            <button className={styles.button}>
+                    <p>Add to Cart</p>
+            </button> 
+      </div>
         </div>
+        </div>
+     
     )
 }
 
